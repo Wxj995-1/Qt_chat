0742373dea4f5c3bc65be51a7dff4fe5d54da140
Wxj995-1 <za00011@163.com>
2026-07-31 19:19:32 +0800

    fix:修复mysql_use_result流式结果集未排空问题

--- ChatServer/include/config/ConfigFileReader.hpp
+++ ChatServer/include/config/ConfigFileReader.hpp
@@ -23,7 +23,7 @@ private:
     void  parseLine(char* line);
     char* trimSpace(char* name);
 
-    bool                                m_load_ok;
+    bool                                m_load_ok = false;
     std::map<std::string, std::string>  m_config_map;
     std::string                         m_config_file;
 };
